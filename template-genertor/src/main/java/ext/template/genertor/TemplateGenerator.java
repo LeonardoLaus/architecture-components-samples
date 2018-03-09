d75444781095a5efa8c20d35cc4957ad8e06880e
liuhao <767041809@qq.com>
2018-03-09 18:41:37 +0800

    update.

--- template-genertor/src/main/java/ext/template/genertor/TemplateGenerator.java
+++ template-genertor/src/main/java/ext/template/genertor/TemplateGenerator.java
@@ -1,10 +1,12 @@
 package ext.template.genertor;
 
 
+import java.io.File;
 import java.io.IOException;
 
 import freemarker.template.Configuration;
 import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.TemplateNotFoundException;
 
 /**
  * Created by roothost on 2018/3/8.
@@ -23,9 +25,13 @@ public class TemplateGenerator {
         config.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
         try {
             config.getTemplate("entity.ftl");
+        }catch (TemplateNotFoundException e) {
+            e.printStackTrace();
+            File dir = new File("src/main/resources/");
+            System.out.println("" + dir.exists());
+            System.out.println("template not found entity.ftl");
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("template not found entity.ftl");
         }
         return config;
     }
